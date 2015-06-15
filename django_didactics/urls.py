83a7692f7c809bfc6f0ce416c8cea67224aca620
hcsturix74 <admin@admin.it>
2015-06-16 00:23:37 +0200

    fixed compatibility with django 1.7

--- django_didactics/urls.py
+++ django_didactics/urls.py
@@ -1,6 +1,4 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.defaults import *
-from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -18,11 +16,11 @@ urlpatterns = patterns('',
 )
 
 
-if settings.DEBUG:
-    urlpatterns = patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', 
-		{'document_root': settings.STATIC_ROOT}),
-    url(r'', include('django.contrib.staticfiles.urls')),
-) + urlpatterns
+# if settings.DEBUG:
+#     urlpatterns = patterns('',
+#     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+#         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+# 	url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+# 		{'document_root': settings.STATIC_ROOT}),
+#     url(r'', include('django.contrib.staticfiles.urls')),
+# ) + urlpatterns
